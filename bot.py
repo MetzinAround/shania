3a5ceee12ce045f54accdfa57e892dff2aca8ed3
Pj Metz <65838556+MetzinAround@users.noreply.github.com>
2024-07-09 09:02:22 -0700

    sdfghjkl;

--- bot.py
+++ bot.py
@@ -5,25 +5,3 @@ import bot_config as conf
 
 intents = discord.Intents.default()
 intents.message_content = True
-
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    print(f"We have logged in as {client.user}")
-
-@client.event
-async def on_message(message):
-
-    if message.author == client.user:
-        return 
-#this happens if the bot sees $shania anywhere in the discord server. 
-    if "$shania" in message.content.lower():
-        await message.channel.send("Let's Go, Girls")
-        return
-
-    if "$impress" in message.content.lower():
-        await message.channel.send("That don't impress me much")
-        return
-
-client.run(conf.secret_token)
